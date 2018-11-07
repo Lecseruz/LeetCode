1cd4622a55c0d60974b541170320686d11af8fcb
Magomed Gadjiev <gadm@1c.ru>
2018-11-07 18:05:25 +0300

    2 update solution

--- 2/src/main/java/Solution.java
+++ 2/src/main/java/Solution.java
@@ -4,35 +4,56 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        ListNode l1 = solution.new ListNode(3);
-        ListNode l2 = solution.new ListNode(3);
-
+        ListNode l1 = solution.new ListNode(2);
+        ListNode l2 = solution.new ListNode(5);
+        l1.next = solution.new ListNode(4);
+        l1.next.next = solution.new ListNode(3);
+        l2.next = solution.new ListNode(6);
+        l2.next.next = solution.new ListNode(4);
         System.out.print(solution.addTwoNumbers(l1, l2).val);
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode out = null;
-        while(l1.next != null){
-            int result = l1.val + l2.val;
-            out = getListNode(out, result);
-            l1 = l1.next;
-            l2 = l2.next;
-        }
+        ListNode head;
+        int remainder = 0;
         int result = l1.val + l2.val;
-        out = getListNode(out, result);
-        return  out;
-    }
-
-    private ListNode getListNode(ListNode out, int result) {
+        ListNode out;
         if (result >= 10){
-            out = new ListNode(result / 10);
-            ListNode listNode = new ListNode(result % 10);
-            listNode.next = out;
-            out = listNode;
+            head = new ListNode((result + remainder) % 10);
+            remainder = (result + remainder) / 10;
+
+            out = head;
         }else {
-            out = new ListNode(result);
+            head = out = new ListNode(result);
         }
-        return out;
+        if(l1.next != null && l2.next != null)
+        {
+            do
+            {
+                l1 = l1.next;
+                l2 = l2.next;
+                result = l1.val + l2.val + remainder;
+                if (result >= 10)
+                {
+                    ListNode listNode = new ListNode(result % 10);
+                    remainder = result / 10;
+                    out.next = listNode;
+                    out = listNode;
+                }
+                else
+                {
+                    ListNode listNode = new ListNode(result % 10);
+                    out.next = listNode;
+                    out = listNode;
+                    remainder = 0;
+                }
+            }
+            while (l1.next != null);
+        }
+        if (remainder != 0){
+            out.next = new ListNode(remainder);
+        }
+        return head;
     }
 
     public class ListNode {
