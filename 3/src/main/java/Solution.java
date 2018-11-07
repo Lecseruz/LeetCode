9983241b8ef2f105542248e57a7b689f88c60cef
magomedgadjiev <magomedalievic@gmail.com>
2018-11-08 00:24:49 +0300

    3 solve

--- 3/src/main/java/Solution.java
+++ 3/src/main/java/Solution.java
@@ -1,2 +1,37 @@
-public class Solution {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(solution.lengthOfLongestSubstring("abcbdef"));
+        System.out.println(solution.lengthOfLongestSubstring("bbbbbb"));
+        System.out.println(solution.lengthOfLongestSubstring(""));
+        System.out.println(solution.lengthOfLongestSubstring("pkwwef"));
+
+
+    }
+    public int lengthOfLongestSubstring(String s) {
+        int m = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); ++i) {
+            Integer value = map.get(s.charAt(i));
+            if (value != null) {
+                if (map.size() > m) {
+                    m = map.size();
+                }
+                map = map.entrySet().stream().filter(characterIntegerEntry ->
+                        characterIntegerEntry.getValue() > value
+                ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            }
+            map.put(s.charAt(i), i);
+        }
+        if(map.size() > m){
+            m = map.size();
+        }
+        return m;
+    }
 }
