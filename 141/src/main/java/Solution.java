2de4aecc20f1164451d7c22adf429f8e83258dcc
Magomed Gadjiev <gadm@1c.ru>
2018-12-07 16:16:29 +0300

    141 solve

---
+++ 141/src/main/java/Solution.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Definition for singly-linked list. class ListNode { int val; ListNode next; ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution
+{
+    public boolean hasCycle(ListNode head)
+    {
+        Set<ListNode> nodesSeen = new HashSet<>();
+        while (head != null) {
+            if (nodesSeen.contains(head)) {
+                return true;
+            } else {
+                nodesSeen.add(head);
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+    static class ListNode
+    {
+        int val;
+        ListNode next;
+
+        ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+}
