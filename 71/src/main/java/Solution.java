0b31bafa507a670d56cc6d83db287677b11e5e28
Magomed Gadjiev <gadm@1c.ru>
2019-01-25 11:51:35 +0300

    solve 71

--- 71/src/main/java/Solution.java
+++ 71/src/main/java/Solution.java
@@ -1,5 +1,53 @@
-package PACKAGE_NAME;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Solution
+class Solution
 {
+    public static void main(String[] args)
+    {
+//        String path = "/a/./b/../../c/";
+        String path = "/..asdsd/..";
+        Solution solution = new Solution();
+        System.out.println(solution.simplifyPath(path));
+    }
+
+    public String simplifyPath(String path)
+    {
+        LinkedList<String> strings = new LinkedList<>();
+        String[] array = path.split("/");
+        for (String el : array)
+        {
+            if (el.equals("..") && !strings.isEmpty())
+            {
+                strings.pollLast();
+            }
+            else if (isWord(el))
+            {
+                strings.addLast(el);
+            }
+        }
+        if (strings.isEmpty())
+        {
+            return "/";
+        }
+        StringBuilder builder = new StringBuilder();
+        while (!strings.isEmpty())
+        {
+            builder.append("/").append(strings.pollFirst());
+        }
+        return builder.toString();
+    }
+
+    public boolean isWord(String word){
+        if(word.equals("")){
+            return false;
+        }
+        if (word.equals(".")){
+            return false;
+        }
+        if (word.equals("..")){
+            return false;
+        }
+        return true;
+    }
 }
