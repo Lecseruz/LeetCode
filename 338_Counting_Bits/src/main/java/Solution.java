a5dfeacd62a1adb95381373c95424d248bfdd3d2
Гаджиев Магомед Алиевич <Gadzhiev_M@Dept07>
2019-08-25 14:46:09 +0300

    solve 338 counting bits

--- 338_Counting_Bits/src/main/java/Solution.java
+++ 338_Counting_Bits/src/main/java/Solution.java
@@ -1,5 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
-public class Solution
+class Solution
 {
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        Arrays.stream(solution.countBits(0)).forEach(System.out::println);
+    }
+    public int[] countBits(int num)
+    {
+        if(num == 0)
+            return new int[]{0};
+        int[] dp = new int[num + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        int n = 2;
+        for (int i = 2; i <= num; ++i)
+        {
+            if (n * 2 == i)
+                n *= 2;
+            dp[i] = dp[i - n] + 1;
+        }
+        return dp;
+    }
 }
